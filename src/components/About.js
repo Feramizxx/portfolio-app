06963d6a8d314812b59409b06fc9087bf50185c2
Server <server@server.com>
2022-05-19 10:24:24 -0700

    About component changed

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
           <div className='col-lg-6 col-md-6 col-12'>
             <h2 className='text-center'>Wanna Know me?</h2>
             <hr />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Hi, I’ am Feramiz. I am passionate about  writing pure code and creating web apps with JavaScript. I am geniuenly love to learn how to handle some problems and solve them. I am currently looking for the right opportunity to work in an environment that will help me progress into a front-end role. </p>
             <button className='btn mb-5  button w-50'><a href='#' className='buttonContent text-light'>KNOW MORE</a></button>
           </div>
         </div>
